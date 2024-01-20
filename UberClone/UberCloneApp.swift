37d719aa1db2009e38f28bec8adf837f94103b48
Fahad9786 <sheikhfahad2005@gmail.com>
2024-01-20 11:43:48 -0500

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Fahad Sheikh on 2024-01-20.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
